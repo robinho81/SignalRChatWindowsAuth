f6c81ff103c48b0a5064c810e2d25b222413b848
Robin Chalmers <robin.chalmers@cegal.com>
2018-06-28 15:35:23 +0200

    Initial check in

---
+++ SignalRChatWindowsAuth/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChatWindowsAuth.Hubs
+{
+  public class ChatHub : Hub
+  {
+    public async Task SendMessage(string user, string message)
+    {
+      await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+
+    public async Task SendToMe(string user, string message)
+    {
+      // Attempting to send a message to a specific user 
+      // See: https://docs.microsoft.com/en-us/aspnet/core/signalr/groups?view=aspnetcore-2.1
+      await Clients.Users(user).SendAsync("ReceiveMyMessage", message);
+    }
+  }
+}
---
+++ SignalRChatWindowsAuth/Pages/Chat.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignalRChatWindowsAuth.Pages
+{
+  public class ChatModel : PageModel
+  {
+    public string Username { get; set; }
+
+    public void OnGet()
+    {
+      Username = HttpContext.User.Identity.Name;
+    }
+  }
+}
