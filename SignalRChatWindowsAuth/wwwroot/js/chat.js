996797de02a9b12c3b25767bb141d688c10e90e6
Robin Chalmers <robin.chalmers@cegal.com>
2018-06-29 09:43:40 +0200

    Adds handler for user specific messages on the client side.

--- SignalRChatWindowsAuth/wwwroot/js/chat.js
+++ SignalRChatWindowsAuth/wwwroot/js/chat.js
@@ -10,12 +10,20 @@ const connection = new signalR.HubConnectionBuilder()
   .withUrl("/chatHub")
   .build();
 
-connection.on("ReceiveMessage", (user, message) => {
+const receiveMessage = function (user, message) {
   const msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
   const encodedMsg = user + " says " + msg;
   const li = document.createElement("li");
   li.textContent = encodedMsg;
   document.getElementById("messagesList").appendChild(li);
+};
+
+connection.on("ReceiveMessage", (user, message) => {
+  receiveMessage(user, message);
+});
+
+connection.on("ReceiveMyMessage", (user, message) => {
+  receiveMessage(user, message);
 });
 
 connection.start().catch(err => console.error(err.toString()));
